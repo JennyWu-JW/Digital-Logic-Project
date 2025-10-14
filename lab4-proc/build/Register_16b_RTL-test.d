816774320d4166ca6810d3b8282daf35fca17cce
Jenny Wu <151204938+JennyWu-JW@users.noreply.github.com>
2025-10-13 22:50:23 -0400

    Add files via upload

---
+++ lab4-proc/build/Register_16b_RTL-test.d
@@ -0,0 +1,18 @@
+#=========================================================================
+# Makefile dependency fragment
+#=========================================================================
+
+Register_16b_RTL-test: \
+  Register_16b_RTL-test.v \
+  ece2300-test.v \
+  Register_16b_RTL.v \
+
+Register_16b_RTL-test.d: \
+  Register_16b_RTL-test.v \
+  ece2300-test.v \
+  Register_16b_RTL.v \
+
+ece2300-test.v:
+
+Register_16b_RTL.v:
+
