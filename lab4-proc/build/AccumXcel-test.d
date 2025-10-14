c0dde5b705e0ccbd1058fe7cb9bdb813dfeab1b2
Jenny Wu <151204938+JennyWu-JW@users.noreply.github.com>
2025-10-13 23:03:51 -0400

    Add files via upload

---
+++ lab4-proc/build/AccumXcel-test.d
@@ -0,0 +1,66 @@
+#=========================================================================
+# Makefile dependency fragment
+#=========================================================================
+
+AccumXcel-test: \
+  AccumXcel-test.v \
+  ece2300-test.v \
+  AccumXcel.v \
+  TestMemory.v \
+  AccumXcelDpath.v \
+  AccumXcelCtrl.v \
+  tinyrv1.v \
+  Register_RTL.v \
+  Adder_32b_GL.v \
+  Mux2_RTL.v \
+  AdderCarrySelect_8b_GL.v \
+  AdderRippleCarry_4b_GL.v \
+  Mux2_4b_GL.v \
+  FullAdder_GL.v \
+  Mux2_1b_GL.v \
+
+AccumXcel-test.d: \
+  AccumXcel-test.v \
+  ece2300-test.v \
+  AccumXcel.v \
+  TestMemory.v \
+  AccumXcelDpath.v \
+  AccumXcelCtrl.v \
+  tinyrv1.v \
+  Register_RTL.v \
+  Adder_32b_GL.v \
+  Mux2_RTL.v \
+  AdderCarrySelect_8b_GL.v \
+  AdderRippleCarry_4b_GL.v \
+  Mux2_4b_GL.v \
+  FullAdder_GL.v \
+  Mux2_1b_GL.v \
+
+ece2300-test.v:
+
+AccumXcel.v:
+
+TestMemory.v:
+
+AccumXcelDpath.v:
+
+AccumXcelCtrl.v:
+
+tinyrv1.v:
+
+Register_RTL.v:
+
+Adder_32b_GL.v:
+
+Mux2_RTL.v:
+
+AdderCarrySelect_8b_GL.v:
+
+AdderRippleCarry_4b_GL.v:
+
+Mux2_4b_GL.v:
+
+FullAdder_GL.v:
+
+Mux2_1b_GL.v:
+
